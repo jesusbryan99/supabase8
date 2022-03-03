54c7004d691a4f29d3403f2bc7a25f81b67611ee
jesusbryan99 <91440514+jesusbryan99@users.noreply.github.com>
2022-03-02 19:50:54 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './index.css'
+import { useState, useEffect } from 'react'
+import { supabase } from './config/supabaseClient'
+import Auth from './components/Auth'
+import Routes from './components/Routes/Routes'
+
+
+
+export default function Home() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+
+ 
+      {!session ? <Auth /> : <Routes key={session.user.id} session={session} /> } 
+    
+           
+</div>
+  );
+
+
+            <
+            /div>
+        );
+    }}
